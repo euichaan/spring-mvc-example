d9c4891ba5d7f79ea56325a5f685aa355979d173
euichanhwang <pine6971@naver.com>
2023-04-22 19:11:12 +0900

    feat: 상품 등록을 개발한다

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,13 +7,18 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -35,6 +40,48 @@ public class BasicItemController {
 		return "basic/item";
 	}
 
+	@GetMapping("/add")
+	public String addForm() {
+		return "basic/addForm";
+	}
+
+	// @PostMapping("/add")
+	public String addItemV1(@RequestParam String itemName,
+		               @RequestParam int price,
+		               @RequestParam Integer quantity,
+		               Model model) {
+		Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);
+
+		itemRepository.save(item);
+
+		model.addAttribute("item", item);
+
+		return "basic/item";
+	}
+
+	// @PostMapping("/add")
+	public String addItemV2(@ModelAttribute("item") Item item) {
+		itemRepository.save(item);
+		// model.addAttribute("item", item); 자동 추가, 생략 가능
+		log.info("{}", item);
+		return "basic/item";
+	}
+
+	// @PostMapping("/add")
+	public String addItemV3(@ModelAttribute Item item) {
+		// Item -> item
+		itemRepository.save(item);
+		return "basic/item";
+	}
+
+	@PostMapping("/add")
+	public String addItemV4(Item item) {
+		itemRepository.save(item);
+		return "basic/item";
+	}
 
 	@PostConstruct
 	public void init() {
